29cdd2081cf34dc572aad5c660e9ca953048fb77
ryanawal12 <rawal@ucsd.edu>
2025-04-29 15:50:29 -0700

    Fixed

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return false;
+    }
     if (add) {
         const result = 0;
 
@@ -14,7 +17,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -27,9 +30,11 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if (length == 0 || !Array.isArray(prices)){
+        return false;
+    }
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
